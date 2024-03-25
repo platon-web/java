4d141c39b8bf847dbb56bc1eb33ce3e26aca6973
Platon Holovetskyi <platongolov2@email.com>
2024-03-25 19:48:14 +0200

    task 1

--- HW_test/HW_new/src/main/java/org/example/Main.java
+++ HW_test/HW_new/src/main/java/org/example/Main.java
@@ -1,8 +1,56 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введіть кількість елементів у наборі:");
+        int size = scanner.nextInt();
+
+        Random random = new Random();
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            numbers.add(random.nextInt(101) - 50);
+        }
+
+        System.out.println("Набір випадкових чисел:");
+        System.out.println(numbers);
+
+        int evenCount = 0;
+        int oddCount = 0;
+        int zeroCount = 0;
+
+        System.out.println("Введіть значення для порівняння:");
+        int target = scanner.nextInt();
+
+        int targetCount = 0;
+
+        for (int num : numbers) {
+            if (num == 0) {
+                zeroCount++;
+            } else if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+
+            if (num == target) {
+                targetCount++;
+            }
+        }
+
+        System.out.println("Кількість парних чисел: " + evenCount);
+        System.out.println("Кількість непарних чисел: " + oddCount);
+        System.out.println("Кількість чисел, рівних нулю: " + zeroCount);
+        System.out.println("Кількість чисел, рівних " + target + ": " + targetCount);
+
+        scanner.close();
+
 
     }
 }
