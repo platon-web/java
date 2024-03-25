6ba5da74453b71a6f05ad220dc5d05e275ad7f86
Platon Holovetskyi <platongolov2@email.com>
2024-03-25 19:51:07 +0200

    task 2

--- HW_test/HW_new/src/main/java/org/example/Main.java
+++ HW_test/HW_new/src/main/java/org/example/Main.java
@@ -1,56 +1,81 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Введіть кількість елементів у наборі:");
+//        int size = scanner.nextInt();
+//
+//        Random random = new Random();
+//        List<Integer> numbers = new ArrayList<>();
+//        for (int i = 0; i < size; i++) {
+//            numbers.add(random.nextInt(101) - 50);
+//        }
+//
+//        System.out.println("Набір випадкових чисел:");
+//        System.out.println(numbers);
+//
+//        int evenCount = 0;
+//        int oddCount = 0;
+//        int zeroCount = 0;
+//
+//        System.out.println("Введіть значення для порівняння:");
+//        int target = scanner.nextInt();
+//
+//        int targetCount = 0;
+//
+//        for (int num : numbers) {
+//            if (num == 0) {
+//                zeroCount++;
+//            } else if (num % 2 == 0) {
+//                evenCount++;
+//            } else {
+//                oddCount++;
+//            }
+//
+//            if (num == target) {
+//                targetCount++;
+//            }
+//        }
+//
+//        System.out.println("Кількість парних чисел: " + evenCount);
+//        System.out.println("Кількість непарних чисел: " + oddCount);
+//        System.out.println("Кількість чисел, рівних нулю: " + zeroCount);
+//        System.out.println("Кількість чисел, рівних " + target + ": " + targetCount);
+//
+//
+//        scanner.close();
 
-        System.out.println("Введіть кількість елементів у наборі:");
-        int size = scanner.nextInt();
+        //task 2
 
-        Random random = new Random();
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            numbers.add(random.nextInt(101) - 50);
-        }
+        List<String> cities = Arrays.asList("Київ", "Львів", "Харків", "Одеса", "Дніпро", "Запоріжжя", "Київ", "Одеса", "Львів");
 
-        System.out.println("Набір випадкових чисел:");
-        System.out.println(numbers);
+        System.out.println("Всі міста:");
+        System.out.println(cities);
 
-        int evenCount = 0;
-        int oddCount = 0;
-        int zeroCount = 0;
+        System.out.println("Міста довжиною більше шести символів:");
+        cities.stream()
+                .filter(city -> city.length() > 6)
+                .forEach(System.out::println);
 
-        System.out.println("Введіть значення для порівняння:");
-        int target = scanner.nextInt();
+        System.out.println("Введіть назву міста для пошуку:");
+        String searchCity = scanner.nextLine();
+        long count = cities.stream()
+                .filter(city -> city.equalsIgnoreCase(searchCity))
+                .count();
+        System.out.println("Місто \"" + searchCity + "\" зустрічається " + count + " раз(и).");
 
-        int targetCount = 0;
-
-        for (int num : numbers) {
-            if (num == 0) {
-                zeroCount++;
-            } else if (num % 2 == 0) {
-                evenCount++;
-            } else {
-                oddCount++;
-            }
-
-            if (num == target) {
-                targetCount++;
-            }
-        }
-
-        System.out.println("Кількість парних чисел: " + evenCount);
-        System.out.println("Кількість непарних чисел: " + oddCount);
-        System.out.println("Кількість чисел, рівних нулю: " + zeroCount);
-        System.out.println("Кількість чисел, рівних " + target + ": " + targetCount);
+        System.out.println("Введіть букву для пошуку міст:");
+        char letter = scanner.next().charAt(0);
+        long citiesCount = cities.stream()
+                .filter(city -> city.toLowerCase().charAt(0) == Character.toLowerCase(letter))
+                .count();
+        System.out.println("Кількість міст на букву \"" + letter + "\": " + citiesCount);
 
         scanner.close();
-
 
     }
 }
