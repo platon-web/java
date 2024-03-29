2dd68020d6cd99532c828d79930b526aec5ed143
Platon Holovetskyi <platongolov2@email.com>
2024-03-29 20:31:38 +0200

    last commit

--- HW_06-04/src/main/java/org/example/hw_0604/HelloServlet.java
+++ HW_06-04/src/main/java/org/example/hw_0604/HelloServlet.java
@@ -1,11 +1,30 @@
 package org.example.hw_0604;
 
+
+import java.awt.datatransfer.DataFlavor;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import static java.lang.System.out;
+/* hw task connect 5 */
+
+import jakarta.servlet.annotation.WebServlet;
+
+//@WebServlet("/CalculatorServlet")
+/* hw task connect 4 */
+//@WebServlet("/TextAnalysisServlet")
+/* hw task connect 3 */
+//@WebServlet("/CalculateServlet")
+/* hw task connect 2 */
+//@WebServlet("/MaxNumberServlet")
+/* lab task conect */
+//@WebServlet("/SurveyServlet")
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
@@ -13,6 +32,8 @@ public class HelloServlet extends HttpServlet {
     public void init() {
         message = "Hello World!";
     }
+
+    private DataFlavor request;
     //task 1
 //    private static final long serialVersionUID = 1L;
 //
@@ -22,6 +43,139 @@ public class HelloServlet extends HttpServlet {
 //        response.setContentType("text/html");
 //        response.getWriter().println("<h1>Цитата Лінуса Торвальдса:</h1>");
 //        response.getWriter().println("<p>" + QUOTE + "</p>");
+//    }
+
+
+    //task 2
+//    String number1Str = request.getParameter("num1");
+//    String number2Str = request.getParameter("num2");
+//    String number3Str = request.getParameter("num3");
+//
+//    double number1 = Double.parseDouble(number1Str);
+//    double number2 = Double.parseDouble(number2Str);
+//    double number3 = Double.parseDouble(number3Str);
+//
+//    double maxNumber = Math.max(number1, Math.max(number2, number3));
+//
+//        out.println("<html><head><title>Maximum Number</title></head><body>");
+//        out.println("<h2>Entered Numbers:</h2>");
+//        out.println("<p>Number 1: " + number1 + "</p>");
+//        out.println("<p>Number 2: " + number2 + "</p>");
+//        out.println("<p>Number 3: " + number3 + "</p>");
+//        out.println("<h2>Maximum Number:</h2>");
+//        out.println("<p>" + maxNumber + "</p>");
+//        out.println("</body></html>");
+
+    /* task 3 */
+//        String number1Str = request.getParameter("number1");
+//        String number2Str = request.getParameter("number2");
+//        String number3Str = request.getParameter("number3");
+//        double number1 = Double.parseDouble(number1Str);
+//        double number2 = Double.parseDouble(number2Str);
+//        double number3 = Double.parseDouble(number3Str);
+//
+//        String operation = request.getParameter("operation");
+//
+//        double result = 0;
+//        if ("max".equals(operation)) {
+//            result = Math.max(number1, Math.max(number2, number3));
+//        } else if ("min".equals(operation)) {
+//            result = Math.min(number1, Math.min(number2, number3));
+//        } else if ("average".equals(operation)) {
+//            result = (number1 + number2 + number3) / 3;
+//        }
+//
+//        response.setContentType("text/html");
+//        out.println("<html><head><title>Calculation Result</title></head><body>");
+//        out.println("<h2>Entered Numbers:</h2>");
+//        out.println("<p>Number 1: " + number1 + "</p>");
+//        out.println("<p>Number 2: " + number2 + "</p>");
+//        out.println("<p>Number 3: " + number3 + "</p>");
+//        out.println("<h2>Calculation Result:</h2>");
+//        out.println("<p>" + result + "</p>");
+//        out.println("</body></html>");
+
+    /* task 4 */
+//        String text = request.getParameter("text");
+//
+//        List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+//        List<Character> consonants = new ArrayList<>();
+//        List<Character> separators = new ArrayList<>();
+//        int vowelsCount = 0;
+//        int consonantsCount = 0;
+//        int separatorsCount = 0;
+//
+//        for (int i = 0; i < text.length(); i++) {
+//            char ch = text.charAt(i);
+//            if (Character.isLetter(ch)) {
+//                if (vowels.contains(ch)) {
+//                    vowelsCount++;
+//                } else {
+//                    consonantsCount++;
+//                    consonants.add(ch);
+//                }
+//            } else if (Character.isWhitespace(ch)) {
+//            } else {
+//                separatorsCount++;
+//                separators.add(ch);
+//            }
+//        }
+//
+//        response.setContentType("text/html");
+//        out.println("<html><head><title>Text Analysis Results</title></head><body>");
+//        out.println("<h2>Text Analysis Results:</h2>");
+//        out.println("<p>Text: " + text + "</p>");
+//        out.println("<p>Vowels Count: " + vowelsCount + "</p>");
+//        out.println("<p>Vowels: " + vowels.toString() + "</p>");
+//        out.println("<p>Consonants Count: " + consonantsCount + "</p>");
+//        out.println("<p>Consonants: " + consonants.toString() + "</p>");
+//        out.println("<p>Separators Count: " + separatorsCount + "</p>");
+//        out.println("<p>Separators: " + separators.toString() + "</p>");
+//        out.println("</body></html>");
+
+    /* task 5 */
+//        double num1 = Double.parseDouble(request.getParameter("num1"));
+//        double num2 = Double.parseDouble(request.getParameter("num2"));
+//        String operation = request.getParameter("operation");
+//
+//        double result = 0;
+//        String errorMessage = "";
+//        switch (operation) {
+//            case "add":
+//                result = num1 + num2;
+//                break;
+//            case "subtract":
+//                result = num1 - num2;
+//                break;
+//            case "multiply":
+//                result = num1 * num2;
+//                break;
+//            case "divide":
+//                if (num2 != 0) {
+//                    result = num1 / num2;
+//                } else {
+//                    errorMessage = "Error: division by zero!";
+//                }
+//                break;
+//            case "power":
+//                result = Math.pow(num1, num2);
+//                break;
+//            case "percentage":
+//                result = (num1 * num2) / 100;
+//                break;
+//            default:
+//                errorMessage = "Error: invalid operation!";
+//        }
+//
+//        response.setContentType("text/html");
+//        out.println("<html><head><title>Calculator Result</title></head><body>");
+//        if (!errorMessage.isEmpty()) {
+//            out.println("<h2>" + errorMessage + "</h2>");
+//        } else {
+//            out.println("<h2>Calculator Result:</h2>");
+//            out.println("<p>" + num1 + " " + operation + " " + num2 + " = " + result + "</p>");
+//        }
+//        out.println("</body></html>");
 //    }
     public void destroy() {
     }
--- HW_06-04/src/main/webapp/index.jsp
+++ HW_06-04/src/main/webapp/index.jsp
@@ -5,9 +5,73 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<a href="hello-servlet">Hello Servlet</a>
+<%--task hw 2--%>
+<%--<h2>Enter Three Numbers</h2>--%>
+<%--<form action="MaxNumberServlet" method="post">--%>
+<%--    <label for="num1">Number 1:</label>--%>
+<%--    <input type="number" id="num1" name="num1" required><br><br>--%>
+
+<%--    <label for="num2">Number 2:</label>--%>
+<%--    <input type="number" id="num2" name="num2" required><br><br>--%>
+
+<%--    <label for="num3">Number 3:</label>--%>
+<%--    <input type="number" id="num3" name="num3" required><br><br>--%>
+
+<%--    <input type="submit" value="Find Maximum">--%>
+<%--</form>--%>
+
+<%--task hw 3--%>
+<%--<h2>Calculate Numbers</h2>--%>
+<%--<form action="CalculateServlet" method="post">--%>
+<%--    <label for="number1">Number 1:</label><br>--%>
+<%--    <input type="number" id="number1" name="number1" required><br>--%>
+
+<%--    <label for="number2">Number 2:</label><br>--%>
+<%--    <input type="number" id="number2" name="number2" required><br>--%>
+
+<%--    <label for="number3">Number 3:</label><br>--%>
+<%--    <input type="number" id="number3" name="number3" required><br>--%>
+
+<%--    <input type="radio" id="max" name="operation" value="max" checked>--%>
+<%--    <label for="max">Maximum</label><br>--%>
+
+<%--    <input type="radio" id="min" name="operation" value="min">--%>
+<%--    <label for="min">Minimum</label><br>--%>
+
+<%--    <input type="radio" id="average" name="operation" value="average">--%>
+<%--    <label for="average">Average</label><br>--%>
+
+<%--    <input type="submit" value="Calculate">--%>
+<%--</form>--%>
+
+<%--task hw 4--%>
+<%--<h2>Text Analysis</h2>--%>
+<%--<form action="TextAnalysisServlet" method="post">--%>
+<%--    <label for="text">Enter text:</label><br>--%>
+<%--    <textarea id="text" name="text" rows="4" cols="50"></textarea><br>--%>
+<%--    <input type="submit" value="Analyze">--%>
+<%--</form>--%>
+
+<%--task hw 5--%>
+<%--<h2>Calculator</h2>--%>
+<%--<form action="CalculatorServlet" method="post">--%>
+<%--    <label for="num1">Number 1:</label><br>--%>
+<%--    <input type="number" id="num1" name="num1" required><br>--%>
+
+<%--    <label for="num2">Number 2:</label><br>--%>
+<%--    <input type="number" id="num2" name="num2" required><br>--%>
+
+<%--    <label for="operation">Operation:</label><br>--%>
+<%--    <select id="operation" name="operation">--%>
+<%--        <option value="add">Addition (+)</option>--%>
+<%--        <option value="subtract">Subtraction (-)</option>--%>
+<%--        <option value="multiply">Multiplication (*)</option>--%>
+<%--        <option value="divide">Division (/)</option>--%>
+<%--        <option value="power">Exponentiation (^)</option>--%>
+<%--        <option value="percentage">Percentage (%)</option>--%>
+<%--    </select><br>--%>
+
+<%--    <input type="submit" value="Calculate">--%>
+<%--</form>--%>
 </body>
 </html>
